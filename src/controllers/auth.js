d9edea380764f24f8bad856c64c7228985111cb9
guudewie <philippthiessen12@gmail.com>
2025-01-03 15:56:09 +0100

    add name property to input html elements in forms

---
+++ src/controllers/auth.js
@@ -0,0 +1,35 @@
+const asyncHandler = require("express-async-handler");
+const passport = require("passport");
+const bcrypt = require("bcryptjs");
+const { createUser } = require("../models/userModel");
+
+const getLoginForm = asyncHandler(async (req, res, next) => {
+  res.render("./login");
+});
+
+const login = asyncHandler(async (req, res, next) => {
+  res.render("./login");
+});
+
+const getSignupForm = asyncHandler(async (req, res, next) => {
+  res.render("./signup");
+});
+
+const signup = asyncHandler(async (req, res, next) => {
+  bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+    if (err) next(err);
+    await createUser({
+      username: req.body.username,
+      password: hashedPassword,
+    });
+  });
+
+  res.redirect("/login");
+});
+
+module.exports = {
+  getLoginForm,
+  login,
+  getSignupForm,
+  signup,
+};
